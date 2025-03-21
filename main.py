14527d422eb8ef0cba70e89d4467febc2ab20836
ICEtheGODofSARAY <aleksandrtreskin94@gmail.com>
2025-03-21 18:57:26 +0300

    RELEASE

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from Utilits.file_handler import update_cash, registration
 from session import player_loop, dealer_loop
 
-# регистрация
-player_login = registration()
+if __name__ == '__main__':
+    # регистрация
+    player_login = registration()
 
-play_more = "да"
-while play_more == "да":
-    player_bet = int(input("Введите ставку: "))
-    # Цикл игрока
-    player_points = player_loop()
-    if player_points == 21:
-        update_cash(user=player_login, new_cash=player_bet)
-        print("ВЫ ВЫЙГРАЛИ")
-    elif player_points > 21:
-        print("ВЫ ПРОИГРАЛИ")
-        update_cash(user=player_login, new_cash=-player_bet)
-    else:
-        # Цикл дилера
-        dealer_points = dealer_loop(player_points=player_points)
-        if dealer_points > 21:
-            print("ВЫ ВЫЙГРАЛИ")
+    play_more = "да"
+    while play_more == "да":
+        player_bet = int(input("Введите ставку: "))
+        # Цикл игрока
+        player_points = player_loop()
+        if player_points == 21:
             update_cash(user=player_login, new_cash=player_bet)
-        elif dealer_points > player_points:
+            print("ВЫ ВЫЙГРАЛИ")
+        elif player_points > 21:
             print("ВЫ ПРОИГРАЛИ")
             update_cash(user=player_login, new_cash=-player_bet)
-    play_more = input("Хотите играть дальше? (да/нет): ").lower()
+        else:
+            # Цикл дилера
+            dealer_points = dealer_loop(player_points=player_points)
+            if dealer_points > 21:
+                print("ВЫ ВЫЙГРАЛИ")
+                update_cash(user=player_login, new_cash=player_bet)
+            elif dealer_points > player_points:
+                print("ВЫ ПРОИГРАЛИ")
+                update_cash(user=player_login, new_cash=-player_bet)
+        play_more = input("Хотите играть дальше? (да/нет): ").lower()
